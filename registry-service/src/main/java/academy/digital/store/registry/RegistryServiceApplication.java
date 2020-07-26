c6c6032e8de91fe0d84720f4f48fe0e073ef9606
James <developermx2020@gmail.com>
2020-07-26 18:22:46 -0500

    ok

---
+++ registry-service/src/main/java/academy/digital/store/registry/RegistryServiceApplication.java
@@ -0,0 +1,13 @@
+package academy.digital.store.registry;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RegistryServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RegistryServiceApplication.class, args);
+	}
+
+}
